9450ae22d8d1cd74f9aa27712136fca9d974044b
jonx1183 <jonx1183@stud.kea.dk>
2023-10-23 15:19:12 +0200

    Can upload dovs to FB database

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
 import axios from 'axios';
 import { useState } from 'react';
+import { database} from './Firebase';
+import { addDoc, collection } from 'firebase/firestore';
 
 export default function App() {
   const apiKey = "AIzaSyC2RCOa6PiG990HjVY_BHr9ftPErtERWRw";
@@ -9,8 +11,21 @@ export default function App() {
   const signUpUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=";
   const [enteredEmail, setEnteredEmail] = useState("test@m.dk");
   const [enteredPassword, setEnteredPassword] = useState("123456");
+  const [userId, setUserId] = useState("");
+  const [enteredText, setEnteredText] = useState("Type here");
 
 
+  async function addDocument(){
+    try{
+      await addDoc(collection(database, userId),{
+        text: enteredText
+      }) 
+    }catch(error){
+      console.log("Error at addDocument " + error);
+    }
+  }
+  
+  
   async function login(){
     try{
       const response = await axios.post(url + apiKey, {
@@ -18,10 +33,11 @@ export default function App() {
         password:enteredPassword,
         returnSecureToken:true
       })
-      alert("Logged in " + response.data.idToken)
+      console.log("Logged in " + response.data.localId)
+      setUserId(response.data.localId)
 
     }catch(error){
-      alert("Not logged in " + error.response.data.error.errors[0].message)
+      console.log("Not logged in " + error.response.data.error.errors[0].message)
     }
   }
 
@@ -69,6 +85,16 @@ export default function App() {
       <Button
       title='Sign up'
       onPress={signUp}
+      />
+
+
+      <TextInput 
+        onChangeText={newText => setEnteredText(newText)}
+        value={enteredText}
+      />
+      <Button
+      title='Add new document'
+      onPress={addDocument}
       />
       
      
